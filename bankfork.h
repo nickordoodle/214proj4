0878c5e775a9285a5393fb16da0a9c469c53e014
GriffinWood <gbw15@scarletmail.rutgers.edu>
2016-04-30 17:30:03 -0400

    Update bankfork.h

--- bankfork.h
+++ bankfork.h
@@ -4,27 +4,14 @@
 
 
 
-typedef struct Account_{
-
-        char *name;
-        float balance;
-        int isInSession;
-} Account;
-
-
-int open (char* accountName);
-int start (char* accountName);
-int creditAmount(int amount);
-int debitAmount(int amount);
-void balance();
-void finish();
-void exit();
 
 
 int addAccount();
 void printAccounts();
+void clientSession();
+
 
 void* printStatusThread(void* arg);
 void* clientListenerThread(void *arg);
-void* clientSessionThread(void *arg);
-void handleUserCommand(char *command, char *accOrNum, int sockfd);
+void handleClientCommards(char *command, char *accOrNum, int sockfd){
+void handleCustomerCommards(char *command, char *accOrNum, int sockfd){
