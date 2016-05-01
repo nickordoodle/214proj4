aef60956d32e3985832a2cfe7d7075b83bf9b52e
GriffinWood <gbw15@scarletmail.rutgers.edu>
2016-05-01 00:48:41 -0400

    Update bankfork.h

--- bankfork.h
+++ bankfork.h
@@ -10,9 +10,17 @@ void printAccounts();
 void clientSession();
 void exitClient();
 
+void open(char * clientMsg, char* acc);
+void start(char * clientMsg, char* acc);
+void credit(char * clientMsg, char* num);
+void debit(char * clientMsg, char* num);
+void balance(char * clientMsg);
+void finish(char * client Msg);
+void exitClient(char * clientMsg);
 
 
 void* printStatusThread(void* arg);
 void* clientListenerThread(void *arg);
-void handleClientCommards(char *command, char *accOrNum, int sockfd){
-void handleCustomerCommards(char *command, char *accOrNum, int sockfd){
+void handleuserCommands(char *command, char *accOrNum, int sockfd);
+
+
