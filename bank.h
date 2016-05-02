e53cc9cc4029c36f3dcaff11485c850c24eaa3a4
GriffinWood <gbw15@scarletmail.rutgers.edu>
2016-05-02 11:45:32 -0400

    Update bank.h

--- bank.h
+++ bank.h
@@ -1,18 +1,31 @@
+typedef struct Map__{
+        pthread_mutex_t clientMutexes[20];
+        pthread_mutex_t newAccountMutex;
+
+        int accountCount;
+
+        char * name [20];
+        float balance[20];
+        int inuse[20];
+}Map;
 
 
 
 
+void printAccounts();
+void clientSession();
+void exitClient();
 
-
-typedef struct Account_{
-
-        char *name;
-        float balance;
-        int isInSession;
-} Account;
+void print();
+void openfnc(char * clientMsg, char* acc);
+void startfnc(char * clientMsg, char* acc);
+void credit(char * clientMsg, char* num);
+void debit(char * clientMsg, char* num);
+void balance(char * clientMsg);
+void finish(char * clientMsg);
+void exitClient(char * clientMsg);
 
 
 void* printStatusThread(void* arg);
 void* clientListenerThread(void *arg);
-void* clientSessionThread(void *arg);
-void handleUserCommand(char *command, char *accOrNum, int sockfd);
+void handleUserCommands(char *command, char *accOrNum, int sockfd);
